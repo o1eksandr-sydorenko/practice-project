09516ca7f19147548b8baf121e0ca1cb819570a5
Oleksandr Sydorenko <o.sydorenko.work@gmail.com>
2025-02-20 11:22:06 +0200

    clustering tests

---
+++ clustering/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const port = 3000;
+const app = express();
+
+app.get("/heavy", (req, res) => {
+  let total = 0;
+  for (let i = 0; i < 50_000_000; i++) {
+    total++;
+  }
+  res.send(`The result of the CPU intensive task is ${total}\n`);
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+  console.log(`worker pid = ${process.pid}`);
+});
